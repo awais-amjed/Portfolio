eb45f4aa3aa58ec6dee756280a53608633a15fad
Mostafa Ahangarha <ahangarha@riseup.net>
2022-01-24 16:12:39 +0300

    add js functionality to mobile menu

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 const mobileMenu = document.querySelector('.mobile-menu');
+const linksInMobileMenu = document.querySelectorAll('.mobile-menu-content li a');
 const openButton = document.getElementById('open-button');
-const closeButton = document.getElementById('close-button')
+const closeButton = document.getElementById('close-button');
 
 openButton.addEventListener('click', (event)=> {
-    event.preventDefault();
+  event.preventDefault();
+  mobileMenu.style.display = "block"
 })
 
 closeButton.addEventListener('click', (event)=> {
-    event.preventDefault();
+  event.preventDefault();
+  mobileMenu.style.display = "none"
 })
+
+linksInMobileMenu.forEach(link => {
+  link.addEventListener('click', (event)=> {
+    mobileMenu.style.display = "none"
+  })
+});
