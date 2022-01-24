6f7264d302a958648c97d9ae871e372ca89ebd44
Mostafa Ahangarha <ahangarha@riseup.net>
2022-01-24 15:52:43 +0300

    add basic js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const mobileMenu = document.querySelector('.mobile-menu');
+const openButton = document.getElementById('open-button');
+const closeButton = document.getElementById('close-button')
+
+openButton.addEventListener('click', (event)=> {
+    event.preventDefault();
+})
+
+closeButton.addEventListener('click', (event)=> {
+    event.preventDefault();
+})
